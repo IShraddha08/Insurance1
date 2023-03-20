5e23d117f9e2660ccd37e0a226f06bc96adafb0a
grand <grand@Chandu>
2023-03-20 15:56:55 +0530

    Change-Id: I0000000000000000000000000000000000000000 claim bean
    Change-Id: I4705137cc7523238126465d5e0e68ded1aee98a8

---
+++ src/com/insurance/details/claimBean.java
@@ -0,0 +1,47 @@
+package com.insurance.details;
+import java.util.Date;
+public class claimBean {
+	
+	    private String reclaim;
+		private String dparts;
+		private String tdamage;
+		private Date dissue;
+		private int sum; 
+		
+		
+		public String getreclaim() {
+			return reclaim;
+		}
+		public void setreclaim(String reclaim) {
+			this.reclaim = reclaim;
+		}
+		public String getdparts() {
+			return dparts;
+		}
+		public void setdparts(String dparts) {
+			this.dparts = dparts;
+		}
+		public String gettdamage() {
+			return tdamage;
+		}
+		public void settdamage(String tdamage) {
+			this.tdamage = tdamage;
+		}
+			
+		public Date dissue() {
+				return dissue;
+			}
+			public void setdissue(Date dissue) {
+				this.dissue = dissue;
+			}
+			
+		public int getsum() {
+				return sum;
+			}
+			public void setsum(int sum) {
+				this.sum = sum;
+			}
+		
+		}
+
+}
